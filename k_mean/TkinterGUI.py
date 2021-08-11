1b2b7f7aebc2800dbc2efeab3266857fd5db742e
omer1 <omer16t7@gmail.com>
2021-08-11 16:49:48 +0300

    Initial commit

--- k_mean/TkinterGUI.py
+++ k_mean/TkinterGUI.py
@@ -6,6 +6,7 @@ import easygui
 from PIL import Image, ImageTk
 import compressions_k_means
 import threading
+import os
 
 
 def get_image_path():
@@ -33,6 +34,11 @@ def clear_screen():
     compress_button['state'] = DISABLED
     compressed_image['image'] = ""
     progress.stop()
+
+
+    image_details['text'] = ""
+    compressed_image_details['text'] = ""
+    compress_ratio_details['text'] = ""
     # stop open_compressed_image from running
 
 
@@ -65,17 +71,42 @@ def compress():
     loading_label['state'] = NORMAL
 
     # send the image to the compression algorithm
-    compressed_image_string = threading.Thread(target=compressions_k_means.compress, args=(image, result, int(k_means_Entry.get())))
+    compressed_image_string = threading.Thread(target=compressions_k_means.compress,
+                                               args=(image, result, int(k_means_Entry.get())))
     compressed_image_string.start()
     # Wait till the thread is finished running , The main thread is stuck
     # after its returned the name of the saved image as string show it on the screen next to the orig image
     threading.Thread(target=open_compressed_image, args=(result,)).start()
 
+    display_stats(image)
+
+
+def display_stats(image):
+    compressed_image_path = image.filename
+    index = 0
+    for i in range(0, len(compressed_image_path) - 1):
+        if compressed_image_path[i] == '/':
+            index = i
+
+    compressed_image_path = compressed_image_path[0:index] + "compressed_" + str(
+        int(k_means_Entry.get())) + "_colors.png"
+
+    image_file_size = os.path.getsize(image.filename)
+    image_details = Label(root, text="image size: " + str(image_file_size) + " bytes", bg="#FFFFFF").place(x=300, y=450)
+    compressed_image_file_size = os.path.getsize(compressed_image_path)
+    compressed_image_details = Label(root, text="compressed image size: " + str(compressed_image_file_size) + " bytes",
+                                     bg="#FFFFFF").place(x=300, y=470)
+
+    compress_ratio = (image_file_size - compressed_image_file_size) / compressed_image_file_size
+    compress_ratio_details = Label(root, text="compress ratio: " + str(compress_ratio * 100) + " %",
+                                   bg="#FFFFFF").place(x=300, y=490)
+
+
+
 
 root = Tk()
 root.title('K-Means PNG Compression')
 root.iconbitmap("icon.ico")
-
 
 # Building all the widgets
 image_container = Label(root)
@@ -86,14 +117,14 @@ compress_button = Button(root, text="Compress", state=DISABLED, command=compress
 clear_button = Button(root, text="Clear", command=clear_screen)
 exit_button = Button(root, text="Exit", command=root.quit)
 progress = tkinter.ttk.Progressbar(root, orient=HORIZONTAL, length=200, mode='indeterminate')
-loading_label = Label(root, text="Loading...", fg="#2191fb", state=DISABLED)
+loading_label = Label(root, text="loading...", fg="#2191fb")
 k_means_label = Label(root, text="k-means:")
 k_means_Entry = Entry(root, bd=5)
-
+stats_label = Label(root, text="Stats:")
+canvas_stats = Canvas(root, width=200, height=100, bg="#FFFFFF")
 
 # Size of the screen
 root.geometry("700x700")
-
 
 # Placing the widgets
 label.place(relx=0.13)
@@ -105,9 +136,10 @@ clear_button.place(x=430, y=600)
 exit_button.place(x=500, y=600)
 loading_label.place(x=300, y=680)
 progress.place(x=250, y=650)
-k_means_label.place(x=240, y=450)
-k_means_Entry.place(x=300, y=450)
-
+k_means_label.place(x=240, y=400)
+k_means_Entry.place(x=300, y=400)
+stats_label.place(x=260, y=450)
+canvas_stats.place(x=300, y=450)
 
 # Initiate the main loop
 root.mainloop()
