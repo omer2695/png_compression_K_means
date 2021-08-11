ed74754f2234622c992ce589825c2b325a3ae60a
Zeronid <zeronid@gmail.com>
2021-08-11 17:39:31 +0300

    Can send "k-means" field as empty

--- k_mean/TkinterGUI.py
+++ k_mean/TkinterGUI.py
@@ -58,6 +58,9 @@ def open_compressed_image(compress_image):
     compress_button['state'] = NORMAL
     loading_label['state'] = DISABLED
 
+    # Find out what is image
+    display_stats(image)
+
 
 def compress():
     result = ["null"]
@@ -66,13 +69,15 @@ def compress():
     compress_button['state'] = DISABLED
     upload_image_button['state'] = DISABLED
     loading_label['state'] = NORMAL
+    compressed_image['image'] = ""
 
     if k_means_Entry.get() != "":
         if k_means_Entry.get().isnumeric() is False or int(k_means_Entry.get()) < 1 or int(k_means_Entry.get()) > 255:
             messagebox.showwarning(title="Error", message="K should be an integer between 1 and 255")
+            return
+        else:
             compressed_image_string = threading.Thread(target=compressions_k_means.compress,
                                                        args=(image, result, int(k_means_Entry.get())))
-            return 0
     else:
         compressed_image_string = threading.Thread(target=compressions_k_means.compress,
                                                    args=(image, result, None))
@@ -81,8 +86,6 @@ def compress():
     # Wait till the thread is finished running , The main thread is stuck
     # after its returned the name of the saved image as string show it on the screen next to the orig image
     threading.Thread(target=open_compressed_image, args=(result,)).start()
-
-    display_stats(image)
 
 
 def display_stats(image):
@@ -98,14 +101,11 @@ def display_stats(image):
             int(k_means_Entry.get())) + "_colors.png"
 
     image_file_size = os.path.getsize(image.filename)
-    image_details = Label(root, text="image size: " + str(image_file_size) + " bytes", bg="#FFFFFF").place(x=300, y=450)
+    image_details['text'] = "image size: " + str(image_file_size) + " bytes"
     compressed_image_file_size = os.path.getsize(compressed_image_path)
-    compressed_image_details = Label(root, text="compressed image size: " + str(compressed_image_file_size) + " bytes",
-                                     bg="#FFFFFF").place(x=300, y=470)
-
+    compressed_image_details['text'] = "compressed image size: " + str(compressed_image_file_size) + " bytes"
     compress_ratio = (image_file_size - compressed_image_file_size) / compressed_image_file_size
-    compress_ratio_details = Label(root, text="compress ratio: " + str(compress_ratio * 100) + " %",
-                                   bg="#FFFFFF").place(x=300, y=490)
+    compress_ratio_details['text'] = "compress ratio: " + str(compress_ratio * 100) + " %"
 
 
 root = Tk()
@@ -126,24 +126,31 @@ k_means_label = Label(root, text="k-means:")
 k_means_Entry = Entry(root, bd=5)
 stats_label = Label(root, text="Stats:")
 canvas_stats = Canvas(root, width=200, height=100, bg="#FFFFFF")
+image_details = Label(root, text="", bg="#FFFFFF")
+compressed_image_details = Label(root, text="", bg="#FFFFFF" )
+compress_ratio_details = Label(root, text="", bg="#FFFFFF")
 
 # Size of the screen
 root.geometry("700x700")
 
-# Placing the widgets
-label.place(relx=0.13)
+
 image_container.place(x=100, y=100)
 compressed_image.place(x=500, y=100)
+label.place(relx=0.13)
 upload_image_button.place(x=180, y=600)
 compress_button.place(x=330, y=600)
 clear_button.place(x=430, y=600)
 exit_button.place(x=500, y=600)
-loading_label.place(x=300, y=680)
 progress.place(x=250, y=650)
+loading_label.place(x=300, y=680)
 k_means_label.place(x=240, y=400)
 k_means_Entry.place(x=300, y=400)
 stats_label.place(x=260, y=450)
 canvas_stats.place(x=300, y=450)
+image_details.place(x=300, y=450)
+compressed_image_details.place(x=300, y=470)
+compress_ratio_details.place(x=300, y=490)
+
 
 # Initiate the main loop
 root.mainloop()
